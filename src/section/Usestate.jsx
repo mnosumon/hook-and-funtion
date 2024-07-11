ffbc45585e9cddc02f00e60d7547fd8eb2f6a21b
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-07-11 14:44:15 +0600

    Counter practice in UseState hook

--- src/section/Usestate.jsx
+++ src/section/Usestate.jsx
@@ -18,11 +18,23 @@ const Usestate = () => {
     inputRef.current.style.background = "blue"
   }
 
+  let [count, setCount] = useState(0)
+  let handleIncriment =()=>{
+    
+    // setCount((count)=>count++) // not work
+    setCount((count)=>count+1)
+  }
+
   return (
     <>
       <div className="bg-red-700 w-[700px] mx-auto p-20">
         <input onChange={handleChange} ref={inputRef} type={ show ? "text" : "password"} placeholder='Password' className='p-5' />
         <button onClick={()=>setShow(!show)} className='bg-slate-700 text-white p-5 ml-3' type='button'>{show ? "Hide" : "show"}</button>
+      </div>
+      <div className="bg-blue-700 w-[700px] mx-auto p-20">
+      <button onClick={handleIncriment} className='bg-slate-700 text-white p-5 ml-3'>Click</button>
+        <h1 className='text-3xl text-white p-5'>{count}</h1>
+        <button onClick={()=>setCount((prev)=>prev - 1)} className='bg-slate-700 text-white p-5 ml-3'>Click</button>
       </div>
     </>
   );
