3dd4249eebc66c795746688765cc322d634cb347
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-07-11 14:26:35 +0600

    useRef hook practice

--- src/section/Usestate.jsx
+++ src/section/Usestate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Usestate = () => {
   let [show, setShow] = useState(false)
@@ -12,10 +12,16 @@ const Usestate = () => {
   //   //   setShow(true)
   //   // }
   // }
+
+  let inputRef = useRef(null)
+  let handleChange = (e)=>{
+    inputRef.current.style.background = "blue"
+  }
+
   return (
     <>
       <div className="bg-red-700 w-[700px] mx-auto p-20">
-        <input type={ show ? "text" : "password"} placeholder='Password' className='p-5' />
+        <input onChange={handleChange} ref={inputRef} type={ show ? "text" : "password"} placeholder='Password' className='p-5' />
         <button onClick={()=>setShow(!show)} className='bg-slate-700 text-white p-5 ml-3' type='button'>{show ? "Hide" : "show"}</button>
       </div>
     </>
