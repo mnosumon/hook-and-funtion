9a4562ebe923cec1177c4cdf799caad0543b1258
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-07-14 23:20:28 +0600

    some changes

--- src/anotherWayContextApi/UserContextApi.jsx
+++ src/anotherWayContextApi/UserContextApi.jsx
@@ -1,16 +1,57 @@
 import { createContext, useState } from 'react';
 
-const UserContext = createContext({});
+export const UserContext = createContext({});
 
-const UserContextProvider = ({ children }) => {
+export const UserContextProvider = ({ children }) => {
     let [userData, setUserData] = useState([]);
 
 
- return (
-   <UserContext.Provider value={{ userData, setUserData  }}>
-     {children}
-   </UserContext.Provider>
- );
+  return (
+    <UserContext.Provider value={{ userData, setUserData  }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
-export { UserContext, UserContextProvider };
+
+
+// import React, { useState } from 'react'
+// import { UserContextApi, UserProvider } from './UserContextApi'
+// import Users from './Users'
+// import NewUser from './NewUser'
+
+
+// const ContextAPI = () => {
+
+// //   let addUser =(newUser)=>{
+// //     setUser((prev)=>[...prev, newUser])
+// // }
+
+//   return (
+//     <>
+//       <UserProvider>
+//         <NewUser />
+//         <Users />
+//       </UserProvider>
+//     </>
+//   )
+// }
+
+// export default ContextAPI
+
+// import { createContext, useState } from "react";
+
+// export const UserContextApi = createContext({})
+
+// export const UserProvider = ({children})=>{
+//     let [user, setUser] = useState([])
+
+//     return(
+//         <UserContextApi.Provider 
+//         value={{
+//         user,setUser
+//         }}>
+//             {children}
+//         </UserContextApi.Provider>
+//     )
+// }
